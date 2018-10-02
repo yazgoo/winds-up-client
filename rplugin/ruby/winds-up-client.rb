76dee4fc33f58d47d208f2241a398366584899ff
yazgoo <yazgoo@gmail.com>
2018-10-02 20:35:04 +0200

    neovim

--- rplugin/ruby/winds-up-client.rb
+++ rplugin/ruby/winds-up-client.rb
@@ -2,7 +2,7 @@ require 'winds-up-client'
 require 'neovim'
 
 Neovim.plugin do |plug|
-  plug.command(:WindsUp) do |nvim|
+  plug.command(:WindsUp, nargs: 1) do |nvim, arg|
     nvim.set_var "windsup", WindsUpClient.new(lpass: true, ultrashort: true).favorites_spots_text.chomp
   end
 end
