a71ae561fc3a7332a3be76c5d71d1ffe70a90f98
yazgoo <yazgoo@gmail.com>
2018-10-10 21:09:58 +0200

    do not reinstantiate client each time

--- rplugin/ruby/winds-up-client.rb
+++ rplugin/ruby/winds-up-client.rb
@@ -1,7 +1,8 @@
 require 'neovim'
 require_relative '../../lib/winds-up-client'
 Neovim.plugin do |plug|
+  client = WindsUpClient.new(lpass: true, ultrashort: true)
   plug.command(:WindsUp) do |nvim|
-    nvim.set_var "windsup", WindsUpClient.new(lpass: true, ultrashort: true).favorites_spots_text.chomp
+    nvim.set_var "windsup", client.favorites_spots_text.chomp
   end
 end
