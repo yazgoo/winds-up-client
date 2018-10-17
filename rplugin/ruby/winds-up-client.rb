ef9b02d84bccd9851881c3e252d872c42dfab68a
yazgoo <yazgoo@gmail.com>
2018-10-17 07:58:47 +0200

    run ruby plugin only once per minute

--- rplugin/ruby/winds-up-client.rb
+++ rplugin/ruby/winds-up-client.rb
@@ -2,7 +2,14 @@ require 'neovim'
 require_relative '../../lib/winds-up-client'
 Neovim.plugin do |plug|
   client = WindsUpClient.new(lpass: true, ultrashort: true)
+  last_check = nil
   plug.command(:WindsUp) do |nvim|
-    nvim.set_var "windsup", client.favorites_spots_text.chomp
+    if last_check.nil? or Time.new - last_check > 60 
+      begin
+        nvim.set_var "windsup", client.favorites_spots_text.chomp
+      rescue Exception
+        last_check = Time.new
+      end
+    end
   end
 end
